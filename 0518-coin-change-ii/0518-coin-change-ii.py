f77cdd37ab029382d05dbe5ffb1de78629f64804
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-01-20 17:26:19 +0900

    Time: 218 ms (57.57%), Space: 31.9 MB (46.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,8 +1,28 @@
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         dp = [0] * (amount + 1) 
+#         dp[0] = 1
+#         for coin in coins :
+#             for i in range(coin, amount + 1) :
+#                 dp[i] += dp[i - coin]
+#         return dp[amount]
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1) 
-        dp[0] = 1
-        for coin in coins :
-            for i in range(coin, amount + 1) :
-                dp[i] += dp[i - coin]
-        return dp[amount]
+        coins.sort()
+        coins = [-1] + coins
+        
+        dp = [[0] * (amount + 1) for _ in range(len(coins))]
+        for i in range(1, len(coins)) :
+            dp[i][0] = 1
+            
+        for i_coin in range(1, len(coins)) :
+            for i_money in range(1, amount + 1) :
+                coin = coins[i_coin]
+                if i_money - coin >= 0 :
+                    dp[i_coin][i_money] = dp[i_coin - 1][i_money] + dp[i_coin][i_money - coin]
+                else :
+                    dp[i_coin][i_money] = dp[i_coin - 1][i_money]
+                    
+        return dp[len(coins) - 1][amount]
+            
