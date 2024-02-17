5141827947acdce7837d4368dae178169ebc39b7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-17 18:46:02 +0900

    Time: 416 ms (40.11%), Space: 37.6 MB (32.13%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,50 +1,65 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        # Dictionary to track the frequencies of elements
-        map = {}
-        for i in arr:
-            map[i] = map.get(i, 0) + 1
+        count = Counter(arr)
+        heap = [(freq, num) for num, freq in count.items()]
+        heapq.heapify(heap)
+        
+        while k > 0 and heap :
+            freq, num = heapq.heappop(heap)
+            k -= freq
+            
+        if k < 0 :
+            heap.append((freq, num))
+            
+        return len(heap)
 
-        n = len(arr)
+# class Solution:
+#     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+#         # Dictionary to track the frequencies of elements
+#         map = {}
+#         for i in arr:
+#             map[i] = map.get(i, 0) + 1
 
-        # List to track the frequencies of frequencies
-        # The maximum possible frequency of any element
-        # will be n, so we'll initialize this list with size n + 1
-        count_of_frequencies = [0] * (n + 1)
+#         n = len(arr)
 
-        # Populating count_of_frequencies list
-        for count in map.values():
-            count_of_frequencies[count] += 1
+#         # List to track the frequencies of frequencies
+#         # The maximum possible frequency of any element
+#         # will be n, so we'll initialize this list with size n + 1
+#         count_of_frequencies = [0] * (n + 1)
 
-        # Variable to track the remaining number of unique elements
-        remaining_unique_elements = len(map)
+#         # Populating count_of_frequencies list
+#         for count in map.values():
+#             count_of_frequencies[count] += 1
 
-        # Traversing over all possible frequencies
-        for i in range(1, n + 1):
-            # For each possible frequency i, we'd like to remove as
-            # many elements with that frequency as possible.
-            # k // i represents the number of maximum possible elements
-            # we could remove with k elements left to remove.
-            # count_of_frequencies[i] represents the actual number of elements
-            # with frequency i.
-            num_elements_to_remove = min(k // i, count_of_frequencies[i])
+#         # Variable to track the remaining number of unique elements
+#         remaining_unique_elements = len(map)
 
-            # Removing the maximum possible elements
-            k -= (i * num_elements_to_remove)
+#         # Traversing over all possible frequencies
+#         for i in range(1, n + 1):
+#             # For each possible frequency i, we'd like to remove as
+#             # many elements with that frequency as possible.
+#             # k // i represents the number of maximum possible elements
+#             # we could remove with k elements left to remove.
+#             # count_of_frequencies[i] represents the actual number of elements
+#             # with frequency i.
+#             num_elements_to_remove = min(k // i, count_of_frequencies[i])
 
-            # num_elements_to_remove is the count of unique elements removed
-            remaining_unique_elements -= num_elements_to_remove
+#             # Removing the maximum possible elements
+#             k -= (i * num_elements_to_remove)
 
-            # If the number of elements that can be removed is less
-            # than the current frequency, we won't be able to remove
-            # any more elements with a higher frequency so we can return
-            # the remaining number of unique elements
-            if k < i:
-                return remaining_unique_elements
+#             # num_elements_to_remove is the count of unique elements removed
+#             remaining_unique_elements -= num_elements_to_remove
 
-        # We have traversed all possible frequencies i.e.,
-        # removed all elements. Returning 0 in this case.
-        return 0
+#             # If the number of elements that can be removed is less
+#             # than the current frequency, we won't be able to remove
+#             # any more elements with a higher frequency so we can return
+#             # the remaining number of unique elements
+#             if k < i:
+#                 return remaining_unique_elements
+
+#         # We have traversed all possible frequencies i.e.,
+#         # removed all elements. Returning 0 in this case.
+#         return 0
         
 # arr 에서  k 개 만큼 제거 하고 남아 있는 수의 개수를 리턴하라
 # 1번 예시의 경우 5가 2개 4가 1개
