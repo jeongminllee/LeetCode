0b273fcc195f1313117add9c2aa3e86136711505
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-19 05:34:41 +0900

    Time: 38 ms (62.64%) | Memory: 16.6 MB (72.25%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,22 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        ans = 0
-        n = len(nums)
-        for i in range(n) :
-            if nums[(i + 1) % n] >= nums[i] :
-                pass
-            else :
-                ans += 1
-                if ans > 1 :
-                    return False
-        return True
+        for i in range(len(nums)) :
+            if nums == sorted(nums) :
+                return True
+            nums = nums[1:] + [nums[0]]
+            print(nums)
+        return False
+
+
+# class Solution:
+#     def check(self, nums: List[int]) -> bool:
+#         ans = 0
+#         n = len(nums)
+#         for i in range(n) :
+#             if nums[(i + 1) % n] >= nums[i] :
+#                 pass
+#             else :
+#                 ans += 1
+#                 if ans > 1 :
+#                     return False
+#         return True
