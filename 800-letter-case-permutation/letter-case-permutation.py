fef278cbadae8f04c371faa0e8053ee0dee5f306
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-27 12:06:22 +0900

    Time: 45 ms (93.28%) | Memory: 17.2 MB (99.29%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,19 +1,27 @@
+# class Solution:
+#     def letterCasePermutation(self, s: str) -> List[str]:
+#         def dfs(i):
+#             if i == len(s):
+#                 result.append(''.join(curr))
+#                 return
+#             if s[i].isalpha():
+#                 curr[i] = s[i].lower()
+#                 dfs(i + 1)
+#                 curr[i] = s[i].upper()
+#                 dfs(i + 1)
+#             else:
+#                 curr[i] = s[i]
+#                 dfs(i + 1)
+
+#         result = []
+#         curr = [''] * len(s)
+#         dfs(0)
+#         return result
+
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        def dfs(i):
-            if i == len(s):
-                result.append(''.join(curr))
-                return
-            if s[i].isalpha():
-                curr[i] = s[i].lower()
-                dfs(i + 1)
-                curr[i] = s[i].upper()
-                dfs(i + 1)
-            else:
-                curr[i] = s[i]
-                dfs(i + 1)
+        output = []
+        for word in s :
+            output.append([word] if word.isdigit() else [word.lower(), word.upper()])
 
-        result = []
-        curr = [''] * len(s)
-        dfs(0)
-        return result
+        return [''.join(w) for w in itertools.product(*output)]
