8ec3a1b70165819796c028cbd4d2df350a06de0c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-27 12:58:26 +0900

    Time: 1139 ms (66.67%) | Memory: 22.1 MB (87.04%) - LeetSync

--- 2109-find-array-given-subset-sums/find-array-given-subset-sums.py
+++ 2109-find-array-given-subset-sums/find-array-given-subset-sums.py
@@ -1,32 +1,26 @@
 class Solution:
     def recoverArray(self, n: int, sums: List[int]) -> List[int]:
-        def dfs(n, sums) :
-            if n == 1 and 0 in sums : 
-                return [max(sums, key = abs)]
-            cands = []
+        ans = []
+        sums.sort()
 
-            d = sums[1] - sums[0]
+        while len(sums) > 1 :
+            num = sums[-1] - sums[-2]
+            countMap = Counter(sums)
+            excluding = []
+            including = []
 
-            for dr in [1, -1] :
-                cnt = Counter(sums)
-                new = []
-                if cnt[0] == 0 :
-                    return []
-                for num in sums[::-dr] :
-                    if cnt[num] == 0 :
-                        continue
-                    if cnt[num - d * dr] == 0 :
-                        break
-                    cnt[num] -= 1
-                    new += [num]
-                    cnt[num - d*dr] -= 1
+            for x in sums :
+                if countMap.get(x) > 0 :
+                    excluding.append(x)
+                    including.append(x + num)
+                    countMap[x] -= 1
+                    countMap[x + num] -= 1
 
-                if len(new) == 1 << (n - 1) :
-                    cands += [[-d*dr] + dfs(n - 1, new[::-dr])]
+            if 0 in excluding :
+                sums = excluding
+                ans.append(num)
+            else :
+                sums = including
+                ans.append(-num)
 
-            return max(cands, key = len) if cands else []
-
-        sums = sorted(sums)
-        return dfs(n, sums)
-
-
+        return ans
