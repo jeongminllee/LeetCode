7d217d6420c96828e4a59c46be754234323bddf0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-27 12:06:36 +0900

    Time: 59 ms (37.34%) | Memory: 18.1 MB (36.39%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,27 +1,27 @@
-# class Solution:
-#     def letterCasePermutation(self, s: str) -> List[str]:
-#         def dfs(i):
-#             if i == len(s):
-#                 result.append(''.join(curr))
-#                 return
-#             if s[i].isalpha():
-#                 curr[i] = s[i].lower()
-#                 dfs(i + 1)
-#                 curr[i] = s[i].upper()
-#                 dfs(i + 1)
-#             else:
-#                 curr[i] = s[i]
-#                 dfs(i + 1)
-
-#         result = []
-#         curr = [''] * len(s)
-#         dfs(0)
-#         return result
-
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        output = []
-        for word in s :
-            output.append([word] if word.isdigit() else [word.lower(), word.upper()])
+        def dfs(i):
+            if i == len(s):
+                result.append(''.join(curr))
+                return
+            if s[i].isalpha():
+                curr[i] = s[i].lower()
+                dfs(i + 1)
+                curr[i] = s[i].upper()
+                dfs(i + 1)
+            else:
+                curr[i] = s[i]
+                dfs(i + 1)
 
-        return [''.join(w) for w in itertools.product(*output)]
+        result = []
+        curr = [''] * len(s)
+        dfs(0)
+        return result
+
+# class Solution:
+#     def letterCasePermutation(self, s: str) -> List[str]:
+#         output = []
+#         for word in s :
+#             output.append([word] if word.isdigit() else [word.lower(), word.upper()])
+
+#         return [''.join(w) for w in itertools.product(*output)]
