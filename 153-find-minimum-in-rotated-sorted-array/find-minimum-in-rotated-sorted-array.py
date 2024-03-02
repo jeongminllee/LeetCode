0339437e7a337c4e95be3d12372a8466bfde084d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 15:38:50 +0900

    Time: 47 ms (30.14%) | Memory: 17 MB (35.78%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,34 +1,17 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        def merge_sort(arr) :
-            if len(arr) <= 1 :
-                return arr
-            mid = len(arr) // 2
-            left = merge_sort(arr[:mid])
-            right = merge_sort(arr[mid:])
+        n = len(nums)
+        l, r = 0, n - 1
 
-            return merge(left, right)
+        while l <= r :
+            mid = (l + r) // 2
+            if nums[mid] < nums[(mid + 1) % n] and nums[mid] < nums[(mid - 1) % n] :
+                return nums[mid]
 
-        def merge(left, right) :
-            result = []
-            i = j = 0
+            elif nums[mid] < nums[r] :
+                r = mid - 1
+            
+            else :
+                l = mid + 1
 
-            while i < len(left) and j < len(right) :
-                if left[i] < right[j] :
-                    result.append(left[i])
-                    i += 1
-                else :
-                    result.append(right[j])
-                    j += 1
-
-            while i < len(left) :
-                result.append(left[i])
-                i += 1
-
-            while j < len(right) :
-                result.append(right[j])
-                j += 1
-
-            return result
-
-        return merge_sort(nums)[0]
+        return nums[0]
