0ffcad988761e227ceb038b5d6549226ea0e67f3
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 11:37:02 +0900

    Time: 127 ms (97.50%) | Memory: 22.9 MB (54.15%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -3,15 +3,18 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        i = m - 1
-        j = 0
+        for i in range(m) :
+            if target >= matrix[i][0] and target <= matrix[i][-1] :
+                left = 0
+                right = n
+                while left <= right :
+                    mid = (left + right) // 2
 
-        while j < n and i >= 0 :
-            if matrix[i][j] == target :
-                return True
-            elif matrix[i][j] > target :
-                i -= 1
-            else :
-                j += 1
+                    if target == matrix[i][mid] :
+                        return True
+                    elif target > matrix[i][mid] :
+                        left = mid + 1
+                    else :
+                        right = mid - 1
 
         return False
