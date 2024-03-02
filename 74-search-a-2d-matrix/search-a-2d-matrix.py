8ac3f3433029b33d5abbf376e724ca79f24fb2c1
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 11:09:54 +0900

    Time: 42 ms (80.08%) | Memory: 17 MB (98.75%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,3 +1,8 @@
+# 2차원 배열 안에 target 이 있냐는 문제임.
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the previous row.
+# 두 가지 조건을 성립하는 m * n 크기의 행렬이 존재한다.
+# 반드시 O(log(m * n))의 시간복잡도를 써야한다.
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         left = 0
