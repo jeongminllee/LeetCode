2f75de21359650a32d0af71f6b5d373c93f30cb5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 10:12:44 +0900

    Time: 1155 ms (61.54%) | Memory: 22.2 MB (83.08%) - LeetSync

--- 2109-find-array-given-subset-sums/find-array-given-subset-sums.py
+++ 2109-find-array-given-subset-sums/find-array-given-subset-sums.py
@@ -1,8 +1,13 @@
+# n의 길이를 가지는 배열 조합을 sums배열의 숫자 중 뽑아서 조합했더니
+# 그 배열의 합이 sums 배열 요소 중 하나인 값을 반환함
+# ex) n = 3, sums = [-3, -2, -1, 0, 0, 1, 2, 3]
+# 이면 저 중 3개를 뽑는다 => [1, 2, -3], 합은 0, sums 배열 내에 0이라는 숫자가 존재.. 정답
+# 만약 [-1, -2, 3]이 었다면? 이도 정답으로 인정... 따라서 중복 정답이 존재한다.
 class Solution:
     def recoverArray(self, n: int, sums: List[int]) -> List[int]:
         ans = []
         sums.sort()
-
+        
         while len(sums) > 1 :
             num = sums[-1] - sums[-2]
             countMap = Counter(sums)
@@ -24,37 +29,3 @@ class Solution:
                 ans.append(-num)
 
         return ans
-        
-
-'''
-class Solution:
-    def recoverArray(self, n: int, sums: List[int]) -> List[int]:
-        def dfs(n, sums) :
-            if n == 1 and 0 in sums : 
-                return [max(sums, key = abs)]
-            cands = []
-
-            d = sums[1] - sums[0]
-
-            for dr in [1, -1] :
-                cnt = Counter(sums)
-                new = []
-                if cnt[0] == 0 :
-                    return []
-                for num in sums[::-dr] :
-                    if cnt[num] == 0 :
-                        continue
-                    if cnt[num - d * dr] == 0 :
-                        break
-                    cnt[num] -= 1
-                    new += [num]
-                    cnt[num - d*dr] -= 1
-
-                if len(new) == 1 << (n - 1) :
-                    cands += [[-d*dr] + dfs(n - 1, new[::-dr])]
-
-            return max(cands, key = len) if cands else []
-
-        sums = sorted(sums)
-        return dfs(n, sums)
-'''
