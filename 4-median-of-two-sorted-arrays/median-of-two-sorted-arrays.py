3029cb4e030b13abcbb0677a5c653b928233ed53
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-03 16:45:28 +0900

    Time: 79 ms (70.10%) | Memory: 16.8 MB (88.09%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2) :
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right :
+            partitionA = (left + right) // 2
+            partitionB = (m + n + 1) // 2 - partitionA
+
+            maxleftA = float('-inf') if partitionA == 0 else nums1[partitionA - 1]
+            minrightA = float('inf') if partitionA == m else nums1[partitionA]
+            maxleftB = float('-inf') if partitionB == 0 else nums2[partitionB - 1]
+            minrightB = float('inf') if partitionB == n else nums2[partitionB]
+
+            if maxleftA <= minrightB and maxleftB <= minrightA :
+                if (m + n) % 2 == 0 :
+                    return (max(maxleftA, maxleftB) + min(minrightA, minrightB)) / 2
+
+                else :
+                    return max(maxleftA, maxleftB)
+
+            elif maxleftA > minrightB :
+                right = partitionA - 1
+            else :
+                left = partitionA + 1
+
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+#         merged_sort = sorted(nums1 + nums2)
+#         n = len(merged_sort)
+#         if n % 2 == 1 :
+#             return merged_sort[n // 2]
+#         else :
+#             return (merged_sort[n // 2 - 1] + merged_sort[n // 2]) / 2
