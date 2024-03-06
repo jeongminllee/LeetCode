226b7373c69c920023837104fd46eae2669bf80e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-06 11:17:34 +0900

    Time: 729 ms (46.99%) | Memory: 20.4 MB (84.43%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,18 +3,44 @@ class Solution:
         nums.sort()
         ans = set()
         for i in range(len(nums) - 2) :
-            j = i + 1
-            k = len(nums) - 1
+            if i > 0 and nums[i] == nums[i - 1] :
+                continue    # Skip duplicate values of the first number
+            j, k = i + 1, len(nums) - 1
             while j < k :
                 target = nums[i] + nums[j] + nums[k]
-
                 if target > 0 :
                     k -= 1
                 elif target < 0 :
                     j += 1
-                else :
+                else : 
                     ans.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
-
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1] :
+                        j += 1  # Skip duplicate values of the second number
+                    while j < k and nums[k] == nums[k + 1] :
+                        k -= 1  # Skip duplicate values of the third number
         return ans
+
+
+# 1. two-pointer approach
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         ans = set()
+#         for i in range(len(nums) - 2) :
+#             j = i + 1
+#             k = len(nums) - 1
+#             while j < k :
+#                 target = nums[i] + nums[j] + nums[k]
+
+#                 if target > 0 :
+#                     k -= 1
+#                 elif target < 0 :
+#                     j += 1
+#                 else :
+#                     ans.add((nums[i], nums[j], nums[k]))
+#                     j += 1
+#                     k -= 1
+
+#         return ans
