908b83179e142caffac8083f2a8d8fac39048446
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-08 16:04:45 +0900

    Time: 40 ms (92.14%) | Memory: 17 MB (48.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,14 @@
 # 문자를 재조합했을때 두 문자가 같아지면 t, 아니면 f
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t)
-        
-        if s == t :
-            return True
-        else :
+        if len(s) != len(t) :
             return False
+
+        s_map = {}
+        t_map = {}
+        for char in s :
+            s_map[char] = s_map.get(char, 0) + 1
+        for char in t :
+            t_map[char] = t_map.get(char, 0) + 1
+        
+        return s_map == t_map
