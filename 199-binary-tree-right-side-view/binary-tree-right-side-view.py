60356937ea2951f203dd396b13fa961550d5eca0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-04 07:31:13 +0900

    Time: 39 ms (38.93%) | Memory: 16.5 MB (45.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -5,23 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        q = deque()
-        q.append(root)
-        ans = []
-
-        while q :
-            r_node = None
-            n = len(q)
-
-            for i in range(n) :
-                node = q.popleft()
-                if node :
-                    r_node = node
-                    q.append(node.left)
-                    q.append(node.right)
-
-            if r_node :
-                ans.append(r_node.val)
-
-        return ans
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        result = []  # Initialize an empty list to store the rightmost values
+        q = collections.deque([root])  # Initialize a deque with the root node
+        
+        # Perform BFS traversal
+        while q:
+            rightMost = None  # Initialize a variable to track the rightmost node at each level
+            n = len(q)  # Get the number of nodes at the current level
+            
+            # Iterate through each node at the current level
+            for i in range(n):
+                node = q.popleft()  # Dequeue a node from the left of the deque
+                if node:
+                    rightMost = node  # Update the rightmost node
+                    q.append(node.left)  # Enqueue the left child
+                    q.append(node.right)  # Enqueue the right child
+            
+            # If a rightmost node was encountered at the current level, append its value to the result
+            if rightMost:
+                result.append(rightMost.val)
+        
+        return result  # Return the list containing the rightmost values
