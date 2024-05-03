1db3c4bbfffbed66887ac1ec06f2d348732d2c9d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-04 07:41:10 +0900

    Time: 34 ms (77.60%) | Memory: 16.7 MB (30.49%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,8 @@
-# [], 1, 2, 3, 3, 4, 5, 6 느낌인가?
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(first = 0, curr = []) :
-            if len(curr) == k :
-                output.append(curr[:])
-                return
+        output = [[]]
 
-            for i in range(first, n) :
-                curr.append(nums[i])
-                backtrack(i + 1, curr)
-                curr.pop()
+        for num in nums :
+            output += [curr + [num] for curr in output]
 
-        output = []
-        n = len(nums)
-        for k in range(n + 1) :
-            backtrack()
         return output
