c98b77eebb4297591c365dfaa3f2ca22e7e3a4f5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-09 12:20:51 +0900

    Time: 379 ms (53.42%) | Memory: 17.1 MB (31.07%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,40 +1,44 @@
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        # Create a frequency array to keep track of the count of each task
-        freq = [0] * 26
-        for task in tasks:
-            freq[ord(task) - ord('A')] += 1
-
-        # Sort the frequency array in non-decreasing order
-        freq.sort()
-        # Calculate the maximum frequency of any task
-        max_freq = freq[25] - 1
-        # Calculate the number of idle slots that will be required
-        idle_slots = max_freq * n
-        # Iterate over the frequency array from the second highest frequency to the lowest frequency
-
-        for i in range(24, -1, -1):
-            # Subtract the minimum of the maximum frequency and the current frequency from the idle slots
-            idle_slots -= min(max_freq, freq[i])
-
-        # If there are any idle slots left, add them to the total number of tasks
-        return idle_slots + len(tasks) if idle_slots > 0 else len(tasks)
-
 # class Solution:
 #     def leastInterval(self, tasks: List[str], n: int) -> int:
+#         # Create a frequency array to keep track of the count of each task
 #         freq = [0] * 26
-#         cnt = 0
+#         for task in tasks:
+#             freq[ord(task) - ord('A')] += 1
 
-#         for task in tasks :
-#             freq[ord(task) - ord("A")] += 1
-#             cnt = max(cnt, freq[ord(task) - ord("A")])
+#         # Sort the frequency array in non-decreasing order
+#         freq.sort()
+#         # Calculate the maximum frequency of any task
+#         max_freq = freq[25] - 1
+#         # Calculate the number of idle slots that will be required
+#         idle_slots = max_freq * n
+#         # Iterate over the frequency array from the second highest frequency to the lowest frequency
 
-#         time = (cnt - 1) * (n + 1)
-#         for f in freq :
-#             if f == cnt :
-#                 time += 1
+#         for i in range(24, -1, -1):
+#             # Subtract the minimum of the maximum frequency and the current frequency from the idle slots
+#             idle_slots -= min(max_freq, freq[i])
 
-#         return max(len(tasks), time)
+#         # If there are any idle slots left, add them to the total number of tasks
+#         return idle_slots + len(tasks) if idle_slots > 0 else len(tasks)
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # freq array to store the frequency of each task
+        freq = [0] * 26
+        mx_cnt = 0
+
+        # Count the frequency of each task and find the maximnum frequency
+        for task in tasks :
+            freq[ord(task) - ord("A")] += 1
+            mx_cnt = max(mx_cnt, freq[ord(task) - ord("A")])
+
+        # Calculate the total time needed for execution
+        time = (mx_cnt - 1) * (n + 1)
+        for f in freq :
+            if f == mx_cnt :
+                time += 1
+
+        # Return the miximum of total time neede and the length of the task list
+        return max(len(tasks), time)
 
 # class Solution:
 #     def leastInterval(self, tasks: List[str], n: int) -> int:
