ccf94b5abe4f4430bc55a12cb07efa312e9a05f6
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-11 21:34:45 +0900

    Time: 46 ms (49.79%) | Memory: 19.5 MB (67.29%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,28 +6,13 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # Initialize an empty stack to perform in-order traversal
-        stack = []
-        # Initialize a variable to count the number of visitied nodes
-        n = 0
-        # Start with the root node
-        temp = root
+        values = []
+        self.inorder(root, values)
+        return values[k - 1]
 
-        # Perform in-order traversal
-        while temp or stack :
-            # Traverse as far left as possible, pushing each node onto the stack
-            while temp :
-                stack.append(temp)
-                temp = temp.left
-            # Once we reach the leftmost node, start popping nodes from the stack
-            temp = stack.pop()
-            # Increment the count of visited nodes
-            n += 1
-            # If count becomes equal to k, return the value of the current node
-            if n == k :
-                return temp.val
-            # Move to the right subtree and continue traversal
-            temp = temp.right
-
-        # Return 0 if kth smallest element not found (shouldn't happen in a valid BST)
-        return 0
+    def inorder(self, root, values) :
+        if root is None :
+            return
+        self.inorder(root.left, values)
+        values.append(root.val)
+        self.inorder(root.right, values)
