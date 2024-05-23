81c258f947e63c286f711f24743bce8b27d91cfc
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-23 22:12:59 +0900

    Time: 219 ms (59.94%) | Memory: 18.2 MB (53.05%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -3,13 +3,13 @@ class Solution:
         if len(arr) == k :
             return arr
 
-        l, r = 0, len(arr) - k
-        while l < r :
-            mid = (l + r) // 2
-            if x - arr[mid] > arr[mid + k] - x :
-                l = mid + 1
+        l, r = 0, len(arr) - 1
+        while r - l >= k :
+            if abs(arr[l] - x) > abs(arr[r] - x) :
+                l += 1
 
             else :
-                r = mid
+                r -= 1
 
-        return arr[l:l+k]
+        return arr[l:r+1
+        ]
