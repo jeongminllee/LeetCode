2f5025bfa5a5e487dbd99c122bf96c98786c8609
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-06 14:51:51 +0900

    Time: 100 ms (83.03%) | Memory: 17.6 MB (38.35%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,14 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        goal = 0
-        ans = 0
-        last = 0
+        goal = ans = last = 0
 
         for i in range(len(nums) - 1) :
-            goal = max(goal, i + nums[i])
+            goal = max(goal, nums[i] + i)
 
             if i == last :
                 last = goal
                 ans += 1
-
         return ans
