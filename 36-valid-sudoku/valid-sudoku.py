bd1cb8dd1fef1c9eff08c80ac411f37da4f765aa
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-01 23:31:22 +0900

    Time: 4 ms (61.35%) | Memory: 16.7 MB (28.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,19 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen = set()
-        
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    row_val = f'({board[i][j]}) found in row {i}'
-                    col_val = f'({board[i][j]}) found in col {j}'
-                    box_val = f'({board[i][j]}) found in box {i // 3}-{j // 3}'
-                    
-                    if row_val in seen or col_val in seen or box_val in seen:
-                        return False
-                    
-                    seen.add(row_val)
-                    seen.add(col_val)
-                    seen.add(box_val)
-        
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxs = defaultdict(set)
+
+        for r in range(9) :
+            for c in range(9) :
+                if board[r][c] == '.' :
+                    continue
+
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxs[(r//3, c//3)] :
+                    return False
+                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxs[(r//3, c//3)].add(board[r][c])
+
         return True
