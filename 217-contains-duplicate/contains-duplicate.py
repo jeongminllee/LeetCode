7a563d67114038ac6344003c706277075b6b1960
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-08 13:51:14 +0900

    Time: 8 ms (68.19%) | Memory: 30.7 MB (71.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,8 +1,14 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         set_nums = set()
+#         for i in range(len(nums)) :
+#             if nums[i] in set_nums :
+#                 return True
+#             set_nums.add(nums[i])
+#         return False
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        set_nums = set()
-        for i in range(len(nums)) :
-            if nums[i] in set_nums :
-                return True
-            set_nums.add(nums[i])
-        return False
+        set_nums = set(nums)
+        
+        return len(set_nums) != len(nums)
