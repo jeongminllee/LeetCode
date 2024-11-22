ee7ef52ec0664f823edd44faae9c2c7642d3e25b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-22 18:16:03 +0900

    Time: 107 ms (28.60%) | Memory: 27.7 MB (53.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,16 +1,15 @@
-# 사각형 이니까 (len(height) - i) * (min(height[left], height[right]))
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        ans = 0
+        max_area = 0
+        left = 0
+        right = len(height) - 1
 
         while left < right :
-            currentArea = min(height[left], height[right]) * (right - left)
-            ans = max(ans, currentArea)
+            max_area = max(max_area, (right - left) * min(height[left], height[right]))
 
             if height[left] < height[right] :
                 left += 1
             else :
                 right -= 1
 
-        return ans
+        return max_area
