7e5b51de9b0fcae4cf8f6c1c0e2e6ce385b6e14a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-16 19:03:10 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (28.83%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,8 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = int(a, 2)
-        b = int(b, 2)
+        decimal_a = decimal_b = 0
+        
+        for i in range(len(a)) :
+            if a[i] == '1' :
+                decimal_a += 1 << (len(a) - i - 1)
 
-        nums = a + b
+        for i in range(len(b)) :
+            if b[i] == '1' :
+                decimal_b += 1 << (len(b) - i - 1)
+
+        # print('decimal_a', decimal_a)
+        # print('decimal_b', decimal_b)
+
+        nums = decimal_a + decimal_b
         res = bin(nums)[2:]
         return res
