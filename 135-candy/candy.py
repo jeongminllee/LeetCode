b383b9c298c13e9d77a03e4cebf3ca0fc058238d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-17 19:16:43 +0900

    Time: 20 ms (30.84%) | Memory: 19.9 MB (70.47%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,29 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        total_candies = n
-        i = 1
+        candies = [1] * n
 
-        while i < n :
-            if ratings[i] == ratings[i-1] :
-                i += 1
-                continue
+        for i in range(1, n) :
+            if ratings[i] > ratings[i - 1] :
+                candies[i] = candies[i - 1] + 1
 
-            current_peak = 0
-            while i < n and ratings[i] > ratings[i-1] :
-                current_peak += 1
-                total_candies += current_peak
-                i += 1
-            
-            if i == n :
-                return total_candies
+        for i in range(n - 2, -1, -1) :
+            if ratings[i] > ratings[i + 1] :
+                candies[i] = max(candies[i], candies[i + 1] + 1)
 
-            current_valley = 0
-            while i < n and ratings[i] < ratings[i - 1] :
-                current_valley += 1
-                total_candies += current_valley
-                i += 1
-            
-            total_candies -= min(current_peak, current_valley)
-
-        return total_candies
+        return sum(candies)
