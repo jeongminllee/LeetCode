880c8f57fca5a8a85044b4725d40399a628b5691
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-19 16:18:37 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (6.74%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,44 +1,44 @@
 class Solution:
-    def intToRoman(self, n: int) -> str:
+    def intToRoman(self, num: int) -> str:
         res = ''
-        while n > 0 :
-            if n >= 1000 :
+        while num > 0 :
+            if num >= 1000 :
                 res += 'M'
-                n -= 1000
-            elif 900 <= n < 1000 :  # < 1000 은 생략해도 될거 같은데 변수 제거 용으로 남겨둠
+                num -= 1000
+            elif 900 <= num < 1000 :  # < 1000 은 생략해도 될거 같은데 변수 제거 용으로 남겨둠
                 res += 'CM'
-                n -= 900
-            elif 500 <= n < 900 :
+                num -= 900
+            elif 500 <= num < 900 :
                 res += 'D'
-                n -= 500
-            elif 400 <= n < 500 :
+                num -= 500
+            elif 400 <= num < 500 :
                 res += 'CD'
-                n -= 400
-            elif 100 <= n < 400 :
+                num -= 400
+            elif 100 <= num < 400 :
                 res += 'C'
-                n -= 100
-            elif 90 <= n < 100 :
+                num -= 100
+            elif 90 <= num < 100 :
                 res += 'XC'
-                n -= 90
-            elif 50 <= n < 90 :
+                num -= 90
+            elif 50 <= num < 90 :
                 res += 'L'
-                n -= 50
-            elif 40 <= n < 50 :
+                num -= 50
+            elif 40 <= num < 50 :
                 res += 'XL'
-                n -= 40
-            elif 10 <= n < 40 :
+                num -= 40
+            elif 10 <= num < 40 :
                 res += 'X'
-                n -= 10
-            elif 9 <= n < 10 :
+                num -= 10
+            elif 9 <= num < 10 :
                 res += 'IX'
-                n -= 9
-            elif 5 <= n < 9 :
+                num -= 9
+            elif 5 <= num < 9 :
                 res += 'V'
-                n -= 5
-            elif 4 <= n < 5 :
+                num -= 5
+            elif 4 <= num < 5 :
                 res += 'IV'
-                n -= 4
-            elif 1 <= n < 4 :
+                num -= 4
+            elif 1 <= num < 4 :
                 res += 'I'
-                n -= 1
+                num -= 1
         return res
