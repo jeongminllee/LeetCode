562602c76191775b7009e50262e78bef2fea5067
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-28 03:30:04 +0900

    Time: 460 ms (33.15%) | Memory: 42.1 MB (28.16%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,13 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        transaction1, transaction2 = 1 << 32, 1 << 32
-        profit1, profit2 = 0, 0
+        # transactions1, transactions2 = 10**5, 10**5
+        # profit1, profit2 = 0, 0
 
-        for i in prices :
-            transaction1 = min(transaction1, i)
-            profit1 = max(profit1, i - transaction1)
+        # for price in prices :
+        #     transactions1 = min(price, transactions1)
+        #     profit1 = max(profit1, price - transactions1)
 
-            transaction2 = min(transaction2, i - profit1)
-            profit2 = max(profit2, i - transaction2)
+        #     transactions2 = min(price - profit1, transactions2)
+        #     profit2 = max(profit2, price - transactions2)
+
+        # return profit2
+
+    
+        dp = [[10**5, 0] * 2 for _ in range(len(prices))]
+
+        for i in range(len(prices)) :
+            dp[i][0] = min(dp[i-1][0], prices[i])
+            dp[i][1] = max(dp[i-1][1], prices[i] - dp[i-1][0])
+            dp[i][2] = min(dp[i-1][2], prices[i] - dp[i-1][1])
+            dp[i][3] = max(dp[i-1][3], prices[i] - dp[i-1][2])
         
-        return profit2
+        return dp[-1][3]
