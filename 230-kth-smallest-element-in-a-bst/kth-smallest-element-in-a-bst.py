33d939bc7f5a7bd8327cc929fb9259181b5b0acd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-04-26 19:12:48 +0900

    Time: 3 ms (36.24%) | Memory: 20.9 MB (93.66%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -7,16 +7,20 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         stack = []
+        print(f'Starting with k = {k}')
 
         while True :
             while root :
                 stack.append(root)
+                print(f'Push to stack : {root.val}')
                 root = root.left
-                
 
             root = stack.pop()
+            print(f'Pop from stack : {root.val}')
             k -= 1
+            print(f'k is now : {k}')
             if k == 0 :
+                print(f'Found answer : {root.val}')
                 return root.val
             
             root = root.right
