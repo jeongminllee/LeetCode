821caa97f4bcf112d1095f781476e77b156a42d7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-05-18 21:40:38 +0900

    Time: 0 ms (100.00%) | Memory: 18.9 MB (74.78%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,39 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums)==0:
+            return [-1,-1]
+        
+        idx = self.binary_search(nums, target)
 
-        def binary_search(nums, target, searching_left) :
-            left = 0
-            right = len(nums) - 1
-            idx = -1
+        if idx == -1 :
+            return [-1, -1]
+        
+        return self.two_pointer(nums, target, idx)
 
-            while left <= right :
-                mid = (left + right) // 2
+    def binary_search(self, nums, target) :
+        left, right = 0, len(nums) - 1
 
-                if nums[mid] > target :
-                    right = mid - 1
-                elif nums[mid] < target :
-                    left = mid + 1
-                else :
-                    idx = mid
-                    if searching_left :
-                        right = mid - 1
-                    else :
-                        left = mid + 1
-
-            return idx
+        while left <= right :
+            mid = (left + right) // 2
             
-        left = binary_search(nums, target, True)
-        right = binary_search(nums, target, False)
+            if target == nums[mid] :
+                return mid  
+            elif target > nums[mid] :
+                left = mid + 1
+            else :
+                right = mid - 1
+        
+        return -1
+
+    def two_pointer(self, nums, target, idx) :
+        left, right = idx, idx
+
+        while 0 <= left and nums[left] == target :
+            left -= 1
+        left += 1
+
+        while right <= len(nums) - 1 and nums[right] == target :
+            right += 1
+        right -= 1
 
         return [left, right]
