9d116f05e649ecf7ad7a051eebfbc6c29eba5b57
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-05-19 19:59:27 +0900

    Time: 0 ms (100.00%) | Memory: 19.1 MB (20.97%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)==0:
+        if len(nums) == 0 :
             return [-1,-1]
         
         idx = self.binary_search(nums, target)
 
         if idx == -1 :
-            return [-1, -1]
-        
+            return [-1,-1]
+
         return self.two_pointer(nums, target, idx)
 
-    def binary_search(self, nums, target) :
-        left, right = 0, len(nums) - 1
+    def binary_search(self, nums: List[int], target: int) -> int :
+        left = 0
+        right = len(nums) - 1
 
         while left <= right :
             mid = (left + right) // 2
-            
-            if target == nums[mid] :
-                return mid  
-            elif target > nums[mid] :
-                left = mid + 1
-            else :
+
+            if nums[mid] == target :
+                return mid
+            elif nums[mid] > target :
                 right = mid - 1
-        
+            else :
+                left = mid + 1
+
         return -1
 
-    def two_pointer(self, nums, target, idx) :
-        left, right = idx, idx
-
+    def two_pointer(self, nums: List[int], target: int, idx: int) -> List[int] :
+        left = right = idx
         while 0 <= left and nums[left] == target :
             left -= 1
         left += 1
