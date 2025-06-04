4d6874cc9eb8198b4bef79de129e7dbe7466e61d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-04 21:43:16 +0900

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.67%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,9 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # nums에서 val과 같지 않은 수가 얼마만큼 포함되어 있는지 찾는 문제
+        # + nums 배열을 0번부터 위 조건에 해당되는 수로 나열
         i = 0
         for j in range(len(nums)) :
             if val != nums[j] :
                 nums[i] = nums[j]
                 i += 1
-    
+        
         return i
