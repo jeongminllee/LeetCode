35600dd67901776fae99f2c7b1459d567e1689d9
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-04 22:01:04 +0900

    Time: 0 ms (100.00%) | Memory: 18.2 MB (31.70%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,15 +1,16 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        n = len(nums)
-        l, r = 0, n - 1
+        # 반드시 시간 O(log n) 으로 풀이할 것 => 여기서 이분탐색 등 을 생각할 수 있었음
+        # 답(최저값)을 몰라도 찾을 수 가 있나...?
+        # 부등호 설정할 때 이상, 이하, 초과, 미만 설정이 어렵다.
+        left = 0
+        right = len(nums) - 1
 
-        if len(nums) == 1 :
-            return nums[0]
-
-        while l < r :
-            mid = (l + r) // 2
-            if nums[mid] > nums[r] :
-                l = mid + 1
+        while left < right :
+            mid = (left + right) // 2
+            if nums[mid] > nums[right] :
+                left = mid + 1
             else :
-                r = mid
-        return nums[l]
+                right = mid
+        
+        return nums[left]
