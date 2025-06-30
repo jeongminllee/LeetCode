2e534b669f25766780d5bb55233150c95bb85abd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-30 17:48:42 +0900

    Time: 60 ms (23.86%) | Memory: 22.2 MB (43.66%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,42 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        targets = [p, q]
-
-        if not root :
-            return None
-        
-        if root in targets :
+        if not root or root == p or root == q :
             return root
 
-        found_left = self.lca(root.left, targets)
-        found_right = self.lca(root.right, targets)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        if found_left and found_right :
+        if left and right :
             return root
-        
-        if found_left :
-            return found_left
-        
-        return found_right
 
-
-    def lca(self, node, targets) :
-        if not node :
-            return None
-        
-        if node in targets :
-            return node
-
-        found_left = self.lca(node.left, targets)
-        found_right = self.lca(node.right, targets)
-
-        if found_left and found_right :
-            return node
-        
-        if found_left :
-            return found_left
-        
-        return found_right
-
-        
+        return left or right
