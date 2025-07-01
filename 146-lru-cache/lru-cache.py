8d76e8cfdfb829f47e862fd1073927d03f555d7d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-07-01 19:57:50 +0900

    Time: 135 ms (62.71%) | Memory: 77.9 MB (99.11%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,49 @@
+class Node :
+    def __init__(self, key, val) :
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.nxt = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dct = {}
-        self.keys = []
+        self.cache = {}
+
+        self.oldest = Node(0, 0)
+        self.latest = Node(0, 0)
+        self.oldest.nxt = self.latest
+        self.latest.prev = self.oldest
 
     def get(self, key: int) -> int:
-        if key in self.keys :
-            self.keys.remove(key)
-            self.keys = [key] + self.keys
-            return self.dct[key]
-
+        if key in self.cache :
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.keys :
-            self.keys.remove(key)
-            self.keys = [key] + self.keys
-            self.dct[key] = value
-            return
+        if key in self.cache :
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-        self.keys = [key] + self.keys
-        self.dct[key] = value
+        if len(self.cache) > self.capacity :
+            lru = self.oldest.nxt
+            self.remove(lru)
+            del self.cache[lru.key]
+    
+    def remove(self, node) :
+        prev, nxt = node.prev, node.nxt
+        prev.nxt = nxt
+        nxt.prev = prev
 
-        if len(self.keys) > self.capacity :
-            del self.dct[self.keys[-1]]
-            self.keys = self.keys[:-1]
-        
+    def insert(self, node) :
+        prev, nxt = self.latest.prev, self.latest
+        prev.nxt = nxt.prev = node
+        node.nxt = nxt
+        node.prev = prev
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
