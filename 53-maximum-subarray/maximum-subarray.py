3a03b4cd94b9f87d1737ed5a78fd26178d229dd7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-07-24 12:15:07 +0900

    Time: 1358 ms (5.06%) | Memory: 31.7 MB (90.02%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,12 +1,24 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = nums[0]   # nums.length가 최소 1이기 때문에 가장 처음 수로 설정
-        tmp = 0
+        if len(nums) == 1 :
+            return nums[0]
 
-        for i in range(len(nums)) :
-            if tmp < 0 :
-                tmp = 0
-            tmp += nums[i]  # 0에다가 nums[i]를 더함
-            res = max(res, tmp)
+        mid = len(nums) // 2
+        left_max = self.maxSubArray(nums[:mid])
+        right_max = self.maxSubArray(nums[mid:])
 
-        return res
+        left_sum = float('-inf')
+        sm = 0
+        for i in range(mid-1, -1, -1) :
+            sm += nums[i]
+            left_sum = max(left_sum, sm)
+
+        right_sum = float('-inf')
+        sm = 0
+        for i in range(mid, len(nums)) :
+            sm += nums[i]
+            right_sum = max(right_sum, sm)
+
+        cross_max = left_sum + right_sum
+
+        return max(left_max, right_max, cross_max)
