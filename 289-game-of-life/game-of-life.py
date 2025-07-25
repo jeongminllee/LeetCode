24af4fb2fb8028bacbd0c92a8a34eb8c7a3cf8c8
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-07-25 14:26:24 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.27%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -3,33 +3,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        dr = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-
         board_copy = copy.deepcopy(board)
 
         n, m = len(board), len(board[0])
 
+        di = [-1, 1, 0, 0, -1, -1, 1, 1]
+        dj = [0, 0, -1, 1, -1, 1, -1, 1]
+
         for i in range(n) :
             for j in range(m) :
-                cnt = 0
-                for di, dj in dr :
-                    ni, nj = i + di, j + dj
+                cnt = 0 # 1의 갯수 세기
+                
+                for d in range(8) :
+                    ni, nj = i + di[d], j + dj[d]
+
                     if 0 <= ni < n and 0 <= nj < m :
                         cnt += board[ni][nj]
                 
-                    live_or_die = 0
-                    if cnt < 2 and board[i][j] == 1 :
-                        live_or_die = 0
-                    elif 2 <= cnt <= 3 and board[i][j] == 1 :
-                        live_or_die = 1
-                    elif cnt > 3 and board[i][j] == 1 :
-                        live_or_die = 0
-                    elif cnt == 3 and board[i][j] == 0 :
-                        live_or_die = 1
-                    else :
-                        live_or_die = board[i][j]
-                    
-                    board_copy[i][j] = live_or_die
+                if cnt < 2 and board[i][j] :
+                    board_copy[i][j] = 0
+                elif 2 <= cnt <= 3 and board[i][j] :
+                    board_copy[i][j] = 1
+                elif cnt > 3 and board[i][j]:
+                    board_copy[i][j] = 0
+                elif cnt == 3 and not board[i][j]:
+                    board_copy[i][j] = 1
+                else :
+                    board_copy[i][j] = board[i][j]
 
         for i in range(n) :
             for j in range(m) :
