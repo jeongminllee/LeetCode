f1246ed0192a4fb7f08c30193e90e541432616bd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-08-04 19:35:20 +0900

    Time: 0 ms (100.00%) | Memory: 18.3 MB (72.33%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,5 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        # 원래는 right = len(nums) - 1 을 해주는데
+        # 가장 큰 값이 들어오게 될 경우를 생각해서 -1 을 하지 않음.
         left, right = 0, len(nums)
 
         while left < right : 
