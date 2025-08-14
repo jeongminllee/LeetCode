0bb097507677301606af6cc5d22a009e8c4a8cc5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-08-14 20:00:06 +0900

    Time: 11 ms (29.54%) | Memory: 21 MB (62.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,13 +3,14 @@ class Solution:
         intervals.sort(key=lambda x:x[0])
         res = []
         temp = [intervals[0][0], intervals[0][1]]
-        for inter in intervals[1:] :
-            if temp[0] <= inter[0] <= temp[1] :
-                temp[0] = min(temp[0], inter[0])
-                temp[1] = max(temp[1], inter[1])
+
+        for interval in intervals[1:] :
+            if temp[0] <= interval[0] <= temp[1] :
+                temp[0] = min(temp[0], interval[0])
+                temp[1] = max(temp[1], interval[1])
             else :
                 res.append(temp)
-                temp = [inter[0], inter[1]]
-
+                temp = interval
+        
         res.append(temp)
         return res
