d44d3fa530ef43385782389d7ce497017373a852
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-08-29 16:50:47 +0900

    Time: 0 ms (100.00%) | Memory: 17.5 MB (98.97%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,35 +1,23 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        '''
-        8자리 문자열로 된 유전자
-        4가지의 문자로 구성되어 있음 = "A", "C", "G", "T"
-        start -> end 로 바꾸려고 함. 
-        2번 예시를 보면 AACCGGTT -> AAACGGTA 로 바꿀려면 몇번 은행에 들어갔다 나와야 되나.
-        한 번 바꿀 때 하나의 문자만 변형할 수 있음. -> 이게 은행에 있어야 바꿀 수 있음.
-        '''
-        bankSet = set(bank)
-        options = ['A', 'C', 'G', 'T']
-
         q = deque()
-        q.append(startGene)
+        q.append((startGene, 0))
 
         visited = set()
         visited.add(startGene)
 
+        options = ['A', 'C', 'G', 'T']
         cnt = 0
 
         while q :
-            for i in range(len(q)) :
-                gene = q.popleft()
-                if gene == endGene :
-                    return cnt
-                for j in range(8) :
-                    for option in options :
-                        newGene = gene[:j] + option + gene[j+1:]
-                        if newGene in bankSet and newGene not in visited :
-                            visited.add(newGene)
-                            q.append(newGene)
+            curr, cnt = q.popleft()
+            if curr == endGene :
+                return cnt
 
-            cnt += 1
-
-        return -1
+            for option in options :
+                for s in range(8) :
+                    nxt = curr[:s] + option + curr[s+1:]
+                    if nxt in bank and nxt not in visited :
+                        visited.add(nxt)
+                        q.append((nxt, cnt + 1))
+        return -1   
