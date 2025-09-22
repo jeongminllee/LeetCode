b0dbee881cd28061ac878ae42f84df613e607480
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-22 16:38:54 +0900

    Time: 13 ms (49.17%) | Memory: 17.9 MB (50.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,14 +2,15 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t) :
             return False
-            
-        dct = {}
-        for s_ in s :
-            dct[s_] = dct.get(s_, 0) + 1
+        
+        words = {}
 
-        for t_ in t :
-            if t_ not in dct or dct[t_] == 0 :
+        for char in s :
+            words[char] = words.get(char, 0) + 1
+        
+        for char in t :
+            if char not in words or words[char] == 0 :
                 return False
-            dct[t_] -= 1
+            words[char] -= 1
         
         return True
