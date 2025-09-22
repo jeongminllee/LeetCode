c721f9e5b11794524ca425996e624d1dd4e258ca
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-22 17:36:22 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (89.81%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -3,14 +3,14 @@ class Solution:
         res = []
 
         for interval in intervals :
-            if newInterval[1] < interval[0] :
+            if newInterval[0] > interval[1] :
+                res.append(interval)
+            elif newInterval[1] < interval[0] :
                 res.append(newInterval)
                 newInterval = interval
-            elif newInterval[0] > interval[1] :
-                res.append(interval)
             else :
                 newInterval[0] = min(interval[0], newInterval[0])
                 newInterval[1] = max(interval[1], newInterval[1])
-
+        
         res.append(newInterval)
         return res
