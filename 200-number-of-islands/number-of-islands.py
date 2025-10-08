acdf3878271daacbc1f4804e03cddc37a90d265e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-10-08 14:47:34 +0900

    Time: 244 ms (61.65%) | Memory: 20.5 MB (37.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,27 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def bfs(si, sj, cnt) :
+            q = deque()
+            q.append((si, sj))
+            v[si][sj] = cnt
+
+            while q :
+                ci, cj = q.popleft()
+                
+                for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
+                    ni, nj = ci + di, cj + dj
+                    if 0<=ni<n and 0<=nj<m and grid[ni][nj] == '1' and v[ni][nj] == 0 :
+                        q.append((ni, nj))
+                        v[ni][nj] = cnt
+
+        n, m = len(grid), len(grid[0])
+        v = [[0] * m for _ in range(n)]
         cnt = 0
-        m, n = len(grid), len(grid[0])
-        v = [[0]*n for _ in range(m)]
 
-        for i in range(m) :
-            for j in range(n) :
-                if grid[i][j] == "1" and v[i][j] == 0  :
-                    q = deque()
-                    q.append((i, j))
-
-                    while q :
-                        x, y = q.popleft()
-                        for d in range(4) :
-                            nx = x + dx[d]
-                            ny = y + dy[d]
-
-                            if 0<=nx<m and 0<=ny<n and grid[nx][ny] == "1" and v[nx][ny] == 0 :
-                                v[nx][ny] = 1
-                                q.append((nx, ny))
-
+        for i in range(n) :
+            for j in range(m) :
+                if grid[i][j] == '1' and v[i][j] == 0 :
                     cnt += 1
+                    bfs(i, j, cnt)
 
         return cnt
