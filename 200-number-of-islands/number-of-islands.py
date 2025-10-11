4d5d0062e71a125426c50707d2c8435e63056142
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-10-11 19:48:06 +0900

    Time: 245 ms (59.73%) | Memory: 20.1 MB (82.25%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,18 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(si, sj, cnt) :
+        def bfs(si: int, sj: int) -> None:
             q = deque()
             q.append((si, sj))
-            v[si][sj] = cnt
+            v[si][sj] = 1
 
             while q :
                 ci, cj = q.popleft()
-                
                 for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
                     ni, nj = ci + di, cj + dj
                     if 0<=ni<n and 0<=nj<m and grid[ni][nj] == '1' and v[ni][nj] == 0 :
                         q.append((ni, nj))
-                        v[ni][nj] = cnt
+                        v[ni][nj] = 1
+
 
         n, m = len(grid), len(grid[0])
         v = [[0] * m for _ in range(n)]
@@ -21,7 +21,7 @@ class Solution:
         for i in range(n) :
             for j in range(m) :
                 if grid[i][j] == '1' and v[i][j] == 0 :
+                    bfs(i, j)
                     cnt += 1
-                    bfs(i, j, cnt)
 
         return cnt
