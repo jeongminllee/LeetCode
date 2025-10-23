be0d64ff0ae132dc02733843e7c661fadb8716fe
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-10-23 18:35:44 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.23%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,25 +1,24 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
-        
         if len(pattern) != len(s) :
             return False
-        
+            
         c2word = {}
         word2c = {}
+
+        for i in range(len(pattern)) :
+            if pattern[i] not in c2word :
+                c2word[pattern[i]] = s[i]
+            else :
+                if c2word[pattern[i]] != s[i] :
+                    return False
         
-        for idx in range(len(s)) :
-            c = pattern[idx]
-            word = s[idx]
-
-            if c not in c2word.keys() :
-                c2word[c] = word
-            elif c2word[c] != word :
-                return False
-
-            if word not in word2c.keys() :
-                word2c[word] = c
-            elif word2c[word] != c :
-                return False
+        for i in range(len(s)) :
+            if s[i] not in word2c :
+                word2c[s[i]] = pattern[i]
+            else :
+                if word2c[s[i]] != pattern[i] :
+                    return False
 
         return True
