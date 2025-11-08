6d521f1aba404c22859bc661d9ce168aa953d94f
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-11-09 05:42:01 +0900

    Time: 4 ms (49.83%) | Memory: 19.6 MB (17.15%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,11 +1,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # INF = 10 ** 8
-        tbl = [[0 for _ in range(i+1)] for i in range(len(triangle))]
+        tbl = [[0 for _ in range(len(triangle[i]))] for i in range(len(triangle))]
         tbl[0][0] = triangle[0][0]
 
         for i in range(1, len(triangle)) :
-            for j in range(i+1) :
+            for j in range(len(triangle[i])) :
                 if j == 0 :
                     tbl[i][j] = tbl[i-1][j] + triangle[i][j]
                 elif j == i :
