e657332bf4fb69f8665398d43cc28219825bac1c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-11-16 23:38:31 +0900

    Time: 7 ms (23.73%) | Memory: 18.5 MB (89.83%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,22 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
-        cur_sum = numbers[l] + numbers[r]
-        
-        while cur_sum != target :
-            if cur_sum > target :
-                r -= 1
+        left, right = 0, len(numbers) - 1
+        while left <= right :
+            if numbers[left] + numbers[right] == target :
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target :
+                right -= 1
             else :
-                l += 1
-            cur_sum = numbers[l] + numbers[r]
-        return [l + 1, r + 1]
-
-# class Solution:
-#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-#         hashmap = {}
-#         for i, n in enumerate(numbers) :
-#             if target - n not in hashmap :
-#                 hashmap[n] = i
-#             else :
-#                 return [hashmap[target-n] + 1, i + 1]
-        
+                left += 1
