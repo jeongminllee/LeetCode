bc424415bebfd37163a444108567df065569c0a5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-01-29 19:18:25 +0900

    Time: 0 ms (100.00%) | Memory: 26.7 MB (19.05%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,22 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        d = k % n
-        nums[:] = nums[-d:] + nums[:-d]
-
-
-# class Solution:
-#     def rotate(self, nums: List[int], k: int) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         n = len(nums)
-#         if k == 0 :
-#             return
-#         elif n > k :
-#             nums[:] = nums[-k:] + nums[:(n - k)]
-#         else :
-#             for i in range(k) :
-#                 rotation = nums[:-1]
-#                 rotation.insert(0, nums[-1])
-#                 nums[:] = rotation
+        k %= n
+        
+        nums[:] = nums[-k:] + nums[:-k]
+        
+        # for _ in range(k) :
+        #     target = nums[-1]
+        #     nums[:] = [target] + nums[:-1]
