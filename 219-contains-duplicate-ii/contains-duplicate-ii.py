5eb36fc9f3d3599e3f7b46b4a8222653ebaaa7b0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-02-10 18:11:09 +0900

    Time: 42 ms (19.62%) | Memory: 32.3 MB (82.54%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        '''
+        # O(n*k) = O(10**5 * 10**5) = O(10**10)
+        # 옳은 풀이는 아님
+        for i in range(len(nums)) :
+            for cnt in range(1, k+1) :
+                j = i + cnt
+                if j <= len(nums) - 1 and nums[i] == nums[j] :
+                    return True
+
+        return False
+        '''
+        '''
+        for i in range(len(nums)) :
+            j = i + k
+            if j > len(nums) :
+                j = len(nums) - 1
+
+            target = nums[i:j+1]
+            set_target = set(target)
+            if len(target) != len(set_target) :
+                return True
+
+        return False
+        '''
+        sliding_window = set()
+
+        for idx in range(len(nums)) :
+            if idx > k :
+                sliding_window.remove(nums[idx-k-1])
+
+            if nums[idx] in sliding_window :
+                return True
+
+            sliding_window.add(nums[idx])
+
+        return False
