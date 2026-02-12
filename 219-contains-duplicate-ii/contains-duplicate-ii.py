8ddb0c101f24a6d0c42aaa5df2c482c1e6ae3771
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-02-12 16:37:57 +0900

    Time: 28 ms (72.60%) | Memory: 36.7 MB (32.43%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,34 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        '''
-        # O(n*k) = O(10**5 * 10**5) = O(10**10)
-        # 옳은 풀이는 아님
-        for i in range(len(nums)) :
-            for cnt in range(1, k+1) :
-                j = i + cnt
-                if j <= len(nums) - 1 and nums[i] == nums[j] :
-                    return True
+        """
+        n = 10 ** 5
+        k = 10 ** 5 
+        O(n) 으로 끝내야함. 
+        """
+        n = len(nums)
+        target = {}
 
-        return False
-        '''
-        '''
-        for i in range(len(nums)) :
-            j = i + k
-            if j > len(nums) :
-                j = len(nums) - 1
-
-            target = nums[i:j+1]
-            set_target = set(target)
-            if len(target) != len(set_target) :
+        for idx in range(n) :
+            if nums[idx] in target and idx - target[nums[idx]] <= k :
                 return True
 
-        return False
-        '''
-        sliding_window = {}
-
-        for idx, val in enumerate(nums) :
-            if val in sliding_window and idx - sliding_window[val] <= k :
-                return True
-            sliding_window[val] = idx
-
+            target[nums[idx]] = idx
         return False
