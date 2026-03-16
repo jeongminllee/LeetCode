9d028491843263bd1c4176a807734e8ca7327051
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-03-16 18:26:38 +0900

    Time: 8 ms (12.74%) | Memory: 19.1 MB (94.31%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,21 +2,20 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dct_s = {}
         dct_t = {}
-        
-        for i in range(len(s)) :
-            if s[i] not in dct_s.keys() :
-                dct_s[s[i]] = t[i]
+
+        for idx in range(len(s)) :
+            if s[idx] not in dct_s.keys() :
+                dct_s[s[idx]] = t[idx]
 
             else :
-                if dct_s[s[i]] != t[i] :
+                if dct_s[s[idx]] != t[idx] :
                     return False
 
-        for i in range(len(t)) :
-            if t[i] not in dct_t.keys() :
-                dct_t[t[i]] = s[i]
-
+        for idx in range(len(t)) :
+            if t[idx] not in dct_t.keys() :
+                dct_t[t[idx]] = s[idx]
             else :
-                if dct_t[t[i]] != s[i] :
+                if dct_t[t[idx]] != s[idx] :
                     return False
-        
+
         return True
