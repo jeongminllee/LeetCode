eadd477fcbbeb38c2474133fee4381dfa76f9b42
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-03-16 11:41:36 +0900

    Time: 3 ms (79.33%) | Memory: 20.9 MB (92.53%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,36 +6,23 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder :
-            return None
-
-        # Create a hashmap to store the indices of elements in the inorder list
         inorder_map = {val : idx for idx, val in enumerate(inorder)}
+        self.idx = 0
 
-        # Define a recursive function to build the binary tree
-        def build(pre_start, pre_end, in_start, in_end) :
-            # Base case: If the start index in the preorder list is greater than the end index,
-            # it indicates that the current subtree is null, so return None
-            if pre_start > pre_end :
+        def build(start, end) :
+            if start > end :
                 return None
 
-            # Extract the value of the root node from the preorder list using the start index
-            root_val = preorder[pre_start]
+            root_val = preorder[self.idx]
+            self.idx += 1
             root = TreeNode(root_val)
 
-            # Find the index of the root node's value in the inorder list using the hashmap
-            in_index = inorder_map[root_val]
+            mid = inorder_map[root_val]
 
-            # Determine the size of the left subtree
-            left_size = in_index - in_start
-
-            # Recursively build the left subtree
-            root.left = build(pre_start + 1, pre_start + left_size, in_start, in_index-1)
-
-            # Recursively build the right subtree
-            root.right = build(pre_start + left_size + 1, pre_end, in_index + 1, in_end)
+            root.left = build(start, mid-1)
+            root.right = build(mid+1, end)
 
             return root
 
-        # Call the build function with initial parameters corresponding to the entire preorder and inorder lists
-        return build(0, len(preorder) - 1, 0, len(inorder) - 1)
+        return build(0, len(preorder)-1)
+        
