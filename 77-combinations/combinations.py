d843fab6cf3be584abacaa24e738c6943c908532
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-03-22 15:33:26 +0900

    Time: 110 ms (51.03%) | Memory: 61.6 MB (23.42%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,21 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        nums = [i for i in range(n+1)]
-        visited = [False for _ in range(n+1)]
         res = []
-        self.combinations(1, k, nums, visited, [], 0, res)
+        output = []
+        def dfs(start) :
+            if len(output) == k :
+                res.append(output[:])
+                return
+            
+            for idx in range(start, n+1):
+                output.append(idx)
+                dfs(idx + 1)
+                output.pop()
 
+        dfs(1)
         return res
-
-    def combinations(self, bgn, end, nums, visited, buildings, depth, res) :
-        if depth == end :
-            res.append(buildings[:])
-            return
-
-        for idx in range(bgn, len(nums)) :
-            if visited[idx] :
-                continue
-
-            visited[idx] = True
-            self.combinations(idx+1, end, nums, visited, buildings + [nums[idx]], depth + 1, res)
-            visited[idx] = False
