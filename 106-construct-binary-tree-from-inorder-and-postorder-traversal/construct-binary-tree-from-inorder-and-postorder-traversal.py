3850b567997d4086245f5acd0bbdab529a20413b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-03-29 16:23:40 +0900

    Time: 7 ms (46.39%) | Memory: 21.1 MB (83.02%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        root_val = postorder.pop()
-        idx = inorder.index(root_val)
-        inorder_left = inorder[:idx]
-        inorder_right = inorder[idx+1:]
+        index_map = {v:i for i, v in enumerate(inorder)}
+        def helper(s, e) :
+            if s > e :
+                return
 
-        root = TreeNode(root_val)
+            el = postorder.pop()
+            si = index_map[el]
+            root = TreeNode(inorder[si])
 
-        def travel(node, postorder, inorder_left, inorder_right) :
-            if postorder and inorder_right :
-                right_val = postorder.pop()
-                node.right = TreeNode(right_val)
-                right_idx = inorder_right.index(right_val)
-                travel(node.right, postorder, inorder_right[:right_idx], inorder_right[right_idx+1:])
+            root.right = helper(si+1, e)
+            root.left = helper(s, si-1)
+            return root
 
-            if postorder and inorder_left :
-                left_val = postorder.pop()
-                node.left = TreeNode(left_val)
-                left_idx = inorder_left.index(left_val)
-                travel(node.left, postorder, inorder_left[:left_idx], inorder_left[left_idx+1:])
-
-        travel(root, postorder, inorder_left, inorder_right)
-        
-        return root
+        return helper(0, len(inorder)-1)
