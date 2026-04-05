8191c97721020bce8499b8a6c52f9123c8cb9c44
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-04-05 14:49:32 +0900

    Time: 47 ms (92.48%) | Memory: 29.7 MB (49.96%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # n <= 10**5 이기 때문에 이중 for문은 좀 위험하고
-        # n ln n 까지 만 가능할듯?
-        # 투포인터로 접근해보자
-        # nxt 가 더 큰 쪽이 먼저 움직이자 양 쪽을 잡아두고
-        left = 0
-        right = len(height) - 1
-        max_value = 0
+        mx = 0
+        left, right = 0, len(height) - 1
 
         while left < right :
-            nxt_value = (right - left) * min(height[right], height[left])
-            max_value = max(max_value, nxt_value)
+            mx = max(mx, (right - left) * min(height[left], height[right]))
 
-            if height[left] < height[right]:
-                left += 1
-            else :
+            if height[left] > height[right] :
                 right -= 1
-
-        return max_value
+            else :
+                left += 1
+        
+        return mx
